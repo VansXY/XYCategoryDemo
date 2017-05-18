b6e1b6d1cf65719dcad379b946294d5ad23d2e2c
肖扬 <xy_dtb@163.com>
2017-05-18 16:54:09 +0800

    first commit

---
+++ XYCategoryDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  XYCategoryDemo
+//
+//  Created by 肖扬 on 2017/5/18.
+//  Copyright © 2017年 肖扬. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ XYCategoryDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  XYCategoryDemo
+//
+//  Created by 肖扬 on 2017/5/18.
+//  Copyright © 2017年 肖扬. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
